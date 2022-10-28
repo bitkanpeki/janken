62c3905d7bf9ce81d081089d95a9a0a49212ccf4
Jonathan Boulay <jonathanboulay@outlook.com>
2022-10-28 11:20:05 +0900

    Complete the game with 5 rounds of play

--- game.js
+++ game.js
@@ -1,38 +1,96 @@
 
-
-
 const getComputerChoice = () => {
 const choices = ['Rock', 'Paper', 'Scissors']
 return choices[Math.floor(Math.random()*3)]
 }
 
-const playRound = (playerSelection, computerSelection) => {
+const sanitizePlayerSelection = (playerSelection) => {
+    return `${playerSelection.slice(0,1).toUpperCase()}${playerSelection.slice(1).toLowerCase()}`
+}
 
+const playRound = (playerSelection, computerSelection) => {
+  
     switch (playerSelection) {
         case 'Rock':
             if (computerSelection === 'Scissors') {
-                console.log('You win')
+                return 'player'
             } else if(computerSelection === 'Paper') {
-                console.log('You lose')
+                return 'computer'
             } else {
-                console.log('It\'s a tie')
+                return 'tie'
             }
-            break;
 
         case 'Paper':
+            if (computerSelection === 'Rock') {
+                return 'player'
+            } else if(computerSelection === 'Scissors') {
+                return 'computer'
+            } else {
+                return 'tie'
+            }
         case 'Scissors':
-            break;
+            if (computerSelection === 'Paper') {
+                return 'player'
+            } else if(computerSelection === 'Rock') {
+                return 'computer'
+            } else {
+                return 'tie'
+            }
         default:
             break;
     }
 
 }
 
-const playerSelection = 'Rock'
+const determineWinner = (playerScore, computerScore) => {
+if (playerScore > computerScore) {
+    return 'You win the GAME!'
+} else if (computerScore > playerScore) {
+    return 'You lose the GAME!'
+} else {
+    return 'The GAME is a tie!'
+}
 
-const computerSelection = getComputerChoice()
+}
 
-playRound(playerSelection, computerSelection)
+
+const game = () => {
+let playerScore = 0
+let computerScore = 0
+
+
+for(let i = 0; i < 5; i++) {
+    let playerSelection = sanitizePlayerSelection(prompt('Enter Rock, Paper, or Scissors'))
+    let computerSelection = getComputerChoice()
+
+    switch (playRound(playerSelection, computerSelection)) {
+        case 'player':
+            console.log('You win')
+            playerScore += 1
+            break
+        case 'computer':
+            console.log('You lose')
+            computerScore += 1
+            break
+        case 'tie':
+            console.log('It\'s a tie')
+            break
+        default:
+            console.log('Wrong input')
+            break;
+    }
+}
+
+console.log(`Score: 
+Player: ${playerScore}
+Computer: ${computerScore}
+`)
+console.log(determineWinner(playerScore, computerScore))
+
+}
+
+game()
+
 
 
 
